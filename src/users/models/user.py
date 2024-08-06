856cffd3c7269320303ac4943b9d0533cf7c86b8
Yaser Khahani <yaserkh@gmail.com>
2024-08-06 17:41:06 +0330

    Write README.md
    - Update Make file.
    - Write ERD using PlantUML.
    - Add missed migration files.
    - Cleanup.

--- src/users/models/user.py
+++ src/users/models/user.py
@@ -10,7 +10,6 @@ class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = None
     email = models.EmailField(unique=True)
-    credit = models.DecimalField(max_digits=10, decimal_places=2, default=100.00)
     updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
@@ -24,5 +23,4 @@ class User(AbstractUser):
     class Meta:
         indexes = [
             models.Index(fields=['email']),
-            models.Index(fields=['credit']),
         ]
